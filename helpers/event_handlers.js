a2e2e2151e8816da133ccddf50ec203d14b34b69
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2023-10-06 00:00:13 +0300

    added size

--- helpers/event_handlers.js
+++ helpers/event_handlers.js
@@ -33,6 +33,32 @@ export function innitialCheckingForm() {
       field.value = localStorage.getItem(field.name);
     }
   }
+
+  const cartOrders = document.querySelector('.cart-order'); //redirect to constants.js
+  const cartItems = JSON.parse(localStorage.getItem('cart'));
+  for (let i = 0; i < cartItems.length; i = i + 1) {
+    const { id, name, imgSrc, price, nmbOfUnits } = cartItems[i];
+    cartOrders.innerHTML += `
+          <div class="ordered-item">
+            <h3>${name}</h3>
+            <div class="item-photo">
+              <img src="${imgSrc}" alt="${name}">
+            </div>
+            <p>order unit: ${nmbOfUnits}</p>
+            <div class="form-group">
+              <label for="size${id}">Select size</label>
+              <select name="Size" id="size${id}" class="sizes" data-id="${id}" required>
+                <option>XS</option>
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+                <option>XXL</option>
+              </select>
+            </div>
+          </div>
+    `;
+  }
 }
 
 export function storeData(e) {
@@ -44,6 +70,13 @@ export function storeData(e) {
     localStorage.setItem(e.target.name, e.target.value);
   }
 }
+
+// export function storeSize(e) {
+//   e.preventDefault();
+
+//   console.log(e.target.dataset.id);
+
+// }
 
 export function resetData(e) {
   e.preventDefault();
@@ -68,13 +101,22 @@ export function createUserInfo() {
   for (let i=0; i<localStorage.length; i = i + 1) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
-    
-    if(key === 'followed') {
+
+    if (key === "cart") {
+      console.log(key); //// need to add logic!!!!!
+    } else if(key === 'followed') {
       const parsedVal = JSON.parse(value) ? 'followed' : `don't followed`;
       userDataHTML += `<p class="check-info">You ${parsedVal} for our daily updates !</p>` 
     } else {
       userDataHTML += `<p class="check-info">${key}: ${value}</p>`
     }
+    
+    // if(key === 'followed') {
+    //   const parsedVal = JSON.parse(value) ? 'followed' : `don't followed`;
+    //   userDataHTML += `<p class="check-info">You ${parsedVal} for our daily updates !</p>` 
+    // } else {
+    //   userDataHTML += `<p class="check-info">${key}: ${value}</p>`
+    // }
   }
 
   contentCheck.innerHTML = userDataHTML;
--- js/order_form.js
+++ js/order_form.js
@@ -1,5 +1,5 @@
 import {LOGO, orderForm, submitBtn, resetBtn} from "../constants/constants.js";
-import { innitialCheckingForm, storeData, goCheckingPage, resetData, goToShopPage} from "../helpers/event_handlers.js";
+import { innitialCheckingForm, storeData,  goCheckingPage, resetData, goToShopPage} from "../helpers/event_handlers.js";
 
 
 // go to home page (clicked logo)
@@ -21,4 +21,35 @@ submitBtn.addEventListener('click', goCheckingPage);
 
 // reset
 resetBtn.addEventListener('click', resetData);
+
+
+document.addEventListener('change', function(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('sizes')) {
+    storeSize(e)
+  }
+})
+
+function storeSize(e) {
+  e.preventDefault();
+
+  const elemID = parseInt(e.target.dataset.id);
+  const size = e.target.value;
+  const itemsArr = JSON.parse(localStorage.getItem("cart"));
+  
+
+  // const currentEl = itemsArr.filter((itemObj) => itemObj.id === elemID).map((product) => {
+  //   return {...product, size: sizeVal}
+  // })
+  // console.log(currentEl);
+
+  for (let i=0; i<itemsArr.length; i= i+1) {
+    if(itemsArr[i].id === elemID) {
+      itemsArr[i] = { ...itemsArr[i], size}
+      console.log(itemsArr[i]);
+      localStorage.setItem("cart", JSON.stringify(itemsArr))
+    }
+  }
+}
+
 
